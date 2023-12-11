8784736ad9303c3a6af22ad54e5f63b4d749a310
trixint <rainbowunicorn@business-insulting.de>
2023-12-11 14:28:48 +0100

    correcting measurementTable, debugging vis.py

--- Benchmark-Models/Froehlich_npjSystBiolAppl2018/vis.py
+++ Benchmark-Models/Froehlich_npjSystBiolAppl2018/vis.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from petab.visualize import plot_with_vis_spec, plot_without_vis_spec
 
 folder = "./"
@@ -7,16 +8,11 @@ condition_file_path = folder + "conditionTable.tsv"
 visualization_file_path = folder + "visualizationTable.tsv"
 simulation_file_path = None
 
-# ax = plot_with_vis_spec(
-#     visualization_file_path,
-#     condition_file_path,
-#     data_file_path,
-#     simulation_file_path,
-# )
-
 ax_without_sim = plot_with_vis_spec(
     visualization_file_path,
     condition_file_path,
     data_file_path
 )
 
+plt.subplot = ax_without_sim.get('plot1')
+plt.show()
